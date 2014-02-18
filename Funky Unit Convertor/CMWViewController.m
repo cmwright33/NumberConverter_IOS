54eec9d3ca036becc85a27a361d7f2b0f2c759b2
Christopher Wright <cmwright33@gmail.com>
2014-02-17 21:38:41 -0500

    using operations

--- Funky Unit Convertor/CMWViewController.m
+++ Funky Unit Convertor/CMWViewController.m
@@ -18,6 +18,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    int x = 5;
+    int y = 20;
+    int z = -2;
+    
+    //operators include: + - * /
+    
+    int additionAnswer  = x + y;
+    int multiplicationAnswer = y * z;
+    
+    float heightOfEverestBaseCamp = 16900.3;
+    float heightOfEverset = 29029;
+    
+    float distanceToTravel = heightOfEverset - heightOfEverestBaseCamp;
+    distanceToTravel = distanceToTravel - 1000;
 }
 
 - (void)didReceiveMemoryWarning
